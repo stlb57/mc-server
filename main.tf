050663c99f95f74b6902af9e93bebf59410626e1
stlb57 <saarthaktyagi1@gmail.com>
2025-08-31 00:08:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 1. Create a Virtual Private Cloud (VPC)
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support   = true
@@ -10,13 +9,11 @@ resource "aws_vpc" "main" {
   tags       = { Name = "main-vpc" }
 }
 
-# 2. Create an Internet Gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
   tags   = { Name = "main-igw" }
 }
 
-# 3. Create a Route Table
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.main.id
 
@@ -28,7 +25,6 @@ resource "aws_route_table" "rt" {
   tags = { Name = "main-rt" }
 }
 
-# 4. Create a Subnet
 resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -36,13 +32,11 @@ resource "aws_subnet" "main" {
   tags                    = { Name = "main-subnet" }
 }
 
-# 5. Associate the Route Table with our Subnet
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.main.id
   route_table_id = aws_route_table.rt.id
 }
 
-# 6. Create a Security Group (Firewall)
 resource "aws_security_group" "autominer_sg" {
   name        = "autominer-sg"
   description = "Allow traffic for Jenkins and Minecraft"
@@ -77,15 +71,13 @@ resource "aws_security_group" "autominer_sg" {
   }
 }
 
-# 7. Manage our SSH Key Pair
 resource "aws_key_pair" "jenkins_key" {
   key_name   = "jenkins-key"
   public_key = file("jenkins-key.pub")
 }
 
-# 8. Create the EC2 Instance (The Server)
 resource "aws_instance" "autominer_server" {
-  ami                    = "ami-0360c520857e3138f" # Ubuntu 24.04 LTS in us-east-1
+  ami                    = "ami-0360c520857e3138f"
   instance_type          = "t2.small"
   key_name               = aws_key_pair.jenkins_key.key_name
   subnet_id              = aws_subnet.main.id
@@ -115,8 +107,6 @@ resource "aws_instance" "autominer_server" {
   }
 }
 
-# Output the server's public IP
 output "public_ip" {
   value = aws_instance.autominer_server.public_ip
-
 }
